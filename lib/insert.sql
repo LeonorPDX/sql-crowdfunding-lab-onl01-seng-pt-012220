2bd0f1ff87d53fe3eeb68482bfd3ac4fe09a555c
Leonor Colbert <leonoracolbert@gmail.com>
2020-03-26 17:33:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,23 @@
-INSERT INTO users (id, name, age) VALUES (1, "Leonor", 31); (2, "Amanda", 29); (3, "Adam", 20); (4, "Joe", 23); (5, "Jane", 23); (6, "John", 24); (7, "Alicia", 40); (8, "Patty", 56); (9, "Frankenstein", 3); (10, "Sabine", 34); (11, "Petra", 23); (12, "Mattias", 45); (13, "Uma", 30); (14, "Garcia", 34); (15, "Kylie", 16); (16, "Lauren", 23); (17, "Reggie", 47); (18, "Carl", 48); (19, "Maxine", 39); (20, "Anita", 36);
+INSERT INTO users (id, name, age) VALUES (1, "Leonor", 31);
+INSERT INTO users (id, name, age) VALUES (2, "Amanda", 29);
+INSERT INTO users (id, name, age) VALUES (3, "Adam", 20);
+INSERT INTO users (id, name, age) VALUES (4, "Joe", 23);
+INSERT INTO users (id, name, age) VALUES (5, "Jane", 23);
+INSERT INTO users (id, name, age) VALUES (6, "John", 24);
+INSERT INTO users (id, name, age) VALUES (7, "Alicia", 40);
+INSERT INTO users (id, name, age) VALUES (8, "Patty", 56);
+INSERT INTO users (id, name, age) VALUES (9, "Frankenstein", 3);
+INSERT INTO users (id, name, age) VALUES (10, "Sabine", 34);
+INSERT INTO users (id, name, age) VALUES (11, "Petra", 23);
+INSERT INTO users (id, name, age) VALUES (12, "Mattias", 45);
+INSERT INTO users (id, name, age) VALUES (13, "Uma", 30);
+INSERT INTO users (id, name, age) VALUES (14, "Garcia", 34);
+INSERT INTO users (id, name, age) VALUES (15, "Kylie", 16);
+INSERT INTO users (id, name, age) VALUES (16, "Lauren", 23);
+INSERT INTO users (id, name, age) VALUES (17, "Reggie", 47);
+INSERT INTO users (id, name, age) VALUES (18, "Carl", 48);
+INSERT INTO users (id, name, age) VALUES (19, "Maxine", 39);
+INSERT INTO users (id, name, age) VALUES (20, "Anita", 36);
 
 
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
