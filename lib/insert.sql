633b09560be7308809f96d1fa32c35b292cc5a02
Leonor Colbert <leonoracolbert@gmail.com>
2020-03-26 17:26:29 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,4 +2,9 @@ INSERT INTO users (id, name, age) VALUES (1, "Leonor", 31); (2, "Amanda", 29); (
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES ();
 
 
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, 'Fyre Festival', 'festival', 10000000, '01-21-2019', '01-30-2020'); (2, 'Harry Potter Tour', 'literature', 120000, '03-30-2019', '02-23-2020'); (3, )
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+(1, 'Fyre Festival', 'festival', 10000000, '01-21-2019', '01-30-2020');
+(2, 'Harry Potter Tour', 'literature', 120000, '03-30-2019', '02-23-2020');
+(3, 'Get me out of student debt', 'education', 85000, '05-30-2017', '12-31-2025');
+(4, 'Kitten needs a new toy', 'animals', 15, '03-23-2020', '04-01-2020');
+(5, 'Publish my thoughts on quarantine', 'literature', 30000, '03-17-2020', '')
