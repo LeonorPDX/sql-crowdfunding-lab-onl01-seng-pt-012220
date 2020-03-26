743effa4f180637d81a8d44bb3c3c5c3afd1a712
Leonor Colbert <leonoracolbert@gmail.com>
2020-03-26 17:34:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -19,5 +19,14 @@ INSERT INTO users (id, name, age) VALUES (18, "Carl", 48);
 INSERT INTO users (id, name, age) VALUES (19, "Maxine", 39);
 INSERT INTO users (id, name, age) VALUES (20, "Anita", 36);
 
-
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, 'Fyre Festival', 'festival', 10000000, '01-21-2019', '01-30-2020');
+(2, 'Harry Potter Tour', 'literature', 120000, '03-30-2019', '02-23-2020');
+(3, 'Get me out of student debt', 'education', 85000, '05-30-2017', '12-31-2025');
+(4, 'Kitten needs a new toy', 'animals', 15, '03-23-2020', '04-01-2020');
+(5, 'Publish my thoughts on quarantine', 'literature', 30000, '03-17-2020', '01-01-2022');
+(6, 'Online schooling for families affected by COVID19', 'education', 400000, '03-25-2020', '10-01-2020');
+(7, 'Houseplants for me', 'selfish', 400, '03-25-2020', '08-01-2020');
+(8, 'Free the nipple', 'education', 4000, '03-15-2020', '05-01-2020');
+(9, 'Waterfront Blues and Jazz Fest', 'festival', 800000, '03-05-2020', '05-01-2021');
+(10, 'Feed Birds Not Squirrels', 'animals', 5000, '03-25-2020', '10-01-2020');
 
