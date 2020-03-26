35d958b7fe425484b87f6f47f566884649dfc79c
Leonor Colbert <leonoracolbert@gmail.com>
2020-03-26 17:36:28 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -20,5 +20,13 @@ INSERT INTO users (id, name, age) VALUES (19, "Maxine", 39);
 INSERT INTO users (id, name, age) VALUES (20, "Anita", 36);
 
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, 'Fyre Festival', 'festival', 10000000, '01-21-2019', '01-30-2020');
-
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (2, 'Harry Potter Tour', 'literature', 120000, '03-30-2019', '02-23-2020');
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (3, 'Get me out of student debt', 'education', 85000, '05-30-2017', '12-31-2025');
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (4, 'Kitten needs a new toy', 'animals', 15, '03-23-2020', '04-01-2020');
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (5, 'Publish my thoughts on quarantine', 'literature', 30000, '03-17-2020', '01-01-2022');
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (6, 'Online schooling for families affected by COVID19', 'education', 400000, '03-25-2020', '10-01-2020');
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (7, 'Houseplants for me', 'selfish', 400, '03-25-2020', '08-01-2020');
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (8, 'Free the nipple', 'education', 4000, '03-15-2020', '05-01-2020');
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (9, 'Waterfront Blues and Jazz Fest', 'festival', 800000, '03-05-2020', '05-01-2021');
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (10, 'Feed Birds Not Squirrels', 'animals', 5000, '03-25-2020', '10-01-2020');
 
